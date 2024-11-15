9d39f79a81471b148b8ad761ace79db99594c54f
Ebigide <ebigidejude@yahoo.com>
2024-11-15 14:58:44 +0100

    Added the hamburger menu display

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -13,3 +13,13 @@ if (modifiedElement) {
     const lastModifiedDate = document.lastModified;
     modifiedElement.textContent = `Last Modified: ${lastModifiedDate}`;
 }
+
+// Store the selected elements that we are going to use.
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listener to the hamburger button
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');  // Toggle the navigation menu
+    hambutton.classList.toggle('show'); // Toggle the hamburger icon (≡ → X)
+});
