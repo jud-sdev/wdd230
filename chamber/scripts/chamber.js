bb7181331d7ad592c025dd95499afcde4e0d73d5
Ebigide <ebigidejude@yahoo.com>
2024-11-17 07:17:58 +0100

    Added lagos chamber of commerce homepage

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,15 @@
+// Define constants for the elements
+const yearElement = document.getElementById("currentYear");
+const modifiedElement = document.getElementById("lastModified");
+
+// Set the current year in the footer
+if (yearElement) {
+    const currentYear = new Date().getFullYear();
+    yearElement.textContent = currentYear;
+}
+
+// Set the last modified date in the footer
+if (modifiedElement) {
+    const lastModifiedDate = document.lastModified;
+    modifiedElement.textContent = `Last Modified: ${lastModifiedDate}`;
+}
